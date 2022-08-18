d02748d88d4d80e01517e3db33eee3b119def908
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2022-08-18 12:04:16 -0300

    18/08/2022

---
+++ src/loginLogout/index.js
@@ -0,0 +1,81 @@
+// https://pt-br.reactjs.org/docs/conditional-rendering.html
+
+import React from 'react';
+import './index.css';
+
+///////////////////////////////////////////////////
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+
+        if(isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        )
+    }
+}
+
+//////////////////////////////////////////
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
+///////////////////////////////////////////
+
+function UserGreeting(props) {
+    return <h1>Bem vindo de volta!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Por favor, se cadastre ou faça login.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+
+    if(isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+
+export default LoginControl;
